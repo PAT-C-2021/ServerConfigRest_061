08af10ea551a53224636d788e67222e6dfd642ca
Bagus Rinu Pangayom <bagusrp2001@gmail.com>
2021-12-30 17:12:22 +0700

    Add project files.

---
+++ ServerConfigRest_061_BagusRinu/Program.cs
@@ -0,0 +1,34 @@
+﻿using ServiceRest_061_BagusRinu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerConfigRest_061_BagusRinu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ServiceHost hostObjek = null;
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY));
+                hostObjek.Open();
+                Console.WriteLine("Server is Ready..");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
